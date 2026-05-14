fe7425da742073dd7cb7ca1a2627b1fd6a3948b3
Prajwal Kamte <kamteprajwal@gmail.com>
2026-05-14 16:51:41 +0530

    chore: remove app/models/__init__.py

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.models.user import User
-from app.models.plot import Plot
-from app.models.customer import Customer
-from app.models.booking import Booking
-from app.models.sale import Sale
-from app.models.payment import Payment
-
-__all__ = ["User", "Plot", "Customer", "Booking", "Sale", "Payment"]
